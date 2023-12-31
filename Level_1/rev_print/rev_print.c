b23c35596c0b6a91f642d854c425e99920cf8e04
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-31 18:10:40 +0000

    Rank 1 : Level 1 : rev_print Done!

---
+++ Level_1/rev_print/rev_print.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+int ft_strlen(char *str);
+
+int main(int argc, char *argv[])
+{
+	int len;
+	if (argc != 2)
+	{
+		write(1, "\n", 1);
+		return 0;
+	}
+	len = ft_strlen(argv[1]);
+	while (len >= 0)
+	{
+		write(1, &argv[1][len], 1);
+		--len;
+	}
+	write(1, "\n", 1);
+	return 0;
+
+}
+
+int ft_strlen(char *str)
+{
+	int i = 0;
+	while (str[i])
+		i++;
+	return i;
+}
