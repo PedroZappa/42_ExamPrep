81adeb39a23e5e5202b0c6ffad1ba05a13c65eb9
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-01 11:42:11 +0000

    Rank 1 : Level 1 : rotone.c Done!

--- Level_1/rotone/rotone.c
+++ Level_1/rotone/rotone.c
@@ -1,2 +1,31 @@
 #include <unistd.h>
 
+void	ft_putchar(char c);
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		ft_putchar('\n');
+		return (1);
+	}
+	char *str = argv[1];
+
+	while (*str)
+	{
+		if ((*str >= 'a' && *str <= 'y') || (*str >= 'A' && *str <= 'Y'))
+			ft_putchar(*str + 1);
+		else if ((*str == 'z') || (*str == 'Z'))
+			ft_putchar(*str - 25);
+		else
+			ft_putchar(*str);
+		++str;
+	}
+	ft_putchar('\n');
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
