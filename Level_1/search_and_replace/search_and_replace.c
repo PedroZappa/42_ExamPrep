72ab720ac6f2ac7e6d281d8df48c314f35f5e11c
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-01 11:58:48 +0000

    Rank 1 : Level 1 : search_and_replace.c Done!

---
+++ Level_1/search_and_replace/search_and_replace.c
@@ -0,0 +1,22 @@
+#include <unistd.h> /* write() */
+#include <stdlib.h> /* exit() */
+
+int main(int argc, char *argv[])
+{
+	char	*str = argv[1];
+	char	search = argv[2][0];
+	char	replace = argv[3][0];
+
+	if ((argc == 4) && !argv[2][1] && !argv[3][1])
+	{
+		while (*str)
+		{
+			if (*str == search)
+				*str = replace;
+			write(1, str, 1);
+			++str;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
