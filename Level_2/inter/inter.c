11e8aa73dbe1ba472591d741f68a0372102e09cf
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-02 14:56:19 +0000

    Rank 2 : Level 2 : inter.c DONE!

---
+++ Level_2/inter/inter.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <stdio.h>
+
+char *ft_strchr(const char *str, char c);
+
+int main(int argc, char **argv)
+{
+	char	ascii[256] = { 0 }; 
+	char	*str1 = NULL;
+	char	*str2 = NULL;
+	int		i;
+	int		j;
+
+	if (argc == 3)
+	{
+		str1 = argv[1];
+		str2 = argv[2];
+
+		i = 0;
+		while(str1[i])
+		{
+			j = 0;
+			while (str2[j])
+			{
+				if ((str1[i] == str2[j]) && (ascii[(int)str1[i]] == 0))
+				{
+					ascii[(int)str1[i]] = 1;
+					write(1, &str1[i], 1);
+				}
+				++j;
+			}
+			++i;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
+
+char *ft_strchr(const char *str, char c)
+{
+	while (*str)
+	{
+		if (*str == c)
+			return ((char *)str);
+		++str;
+	}
+	return (0);
+}
