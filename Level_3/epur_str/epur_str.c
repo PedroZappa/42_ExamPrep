ac712ec5957a7328c287f2b6e05b873234c5a638
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-03 17:08:54 +0000

    Rank 2 : Level 3 : epur_str.c Done!

---
+++ Level_3/epur_str/epur_str.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+
+int ft_isspace(char *str);
+
+int main(int argc, char **argv)
+{
+	char	*str;
+	int		space_flag = 0;
+
+	if (argc == 2)
+	{
+		str = argv[1];
+		while (*str)
+		{
+			while (ft_isspace(str))
+				++str;
+			while (*str && (*str != ' ') && (*str != '\t'))
+			{
+				space_flag = 0;
+				write(1, str++, 1);
+			}
+			if (*str && ft_isspace(str) && !space_flag)
+			{
+				space_flag = 1;
+				if (*(++str) == '\0')
+					break;
+				write(1, " ", 1);
+			}
+			else
+				break;
+			++str;
+		}
+		write(1, "\n", 1);
+	}
+	else
+		write(1, "\n", 1);
+	return (0);
+}
+
+int ft_isspace(char *str)
+{
+	if (*str == ' ' || *str == '\t')
+		return (1);
+	return (0);
+}
