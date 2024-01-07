5712af2c63fac202e8c1469831fe8b287b4ff6e7
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-07 21:56:10 +0000

    Rank 2 : Level 1 : first_word.c Improved

--- Level_1/first_word/first_word.c
+++ Level_1/first_word/first_word.c
@@ -1,20 +1,22 @@
 #include <unistd.h>
 
+int ft_isspace(char c);
+
 int main(int argc, char **argv)
 {
-	if (argc != 2)
+	if (argc == 2)
 	{
-		write(1, "\n", 1);
-		return (0);
-	}
-	char *str = argv[1];
-
-	while (*str && (*str != ' ') && (*str != '\n') && (*str != '\t'))
-	{
-		write(1, str, 1);
-		++str;
+		char *str = argv[1];
+		while (ft_isspace(*str))
+			++str;
+		while (*str && !ft_isspace(*str))
+			write(1, str++, 1);
 	}
 	write(1, "\n", 1);
-
 	return (0);
+}
+
+int ft_isspace(char c)
+{
+	return (c == ' ' || c == '\n' || c == '\t');
 }
