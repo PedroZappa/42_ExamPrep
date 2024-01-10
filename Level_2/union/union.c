984bc47993f9d3c3c4d9591404322c4e2c60b841
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-10 11:09:43 +0000

    Rank 2 : Level 4 : union.c DONE!

---
+++ Level_2/union/union.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	char ascii[256] = { 0 };
+	char *str1;
+	char *str2;
+	int i;
+	
+	if (argc == 3)
+	{
+		str1 = argv[1];
+		str2 = argv[2];
+		
+		i = 0;
+		while (str1[i])
+		{
+			if (ascii[(int)str1[i]] == 0)
+			{
+				ascii[(int)str1[i]] = 1;
+				write(1, &str1[i], 1);
+			}
+			++i;
+		}
+		i = 0;
+		while (str2[i])
+		{
+			if (ascii[(int)str2[i]] == 0)
+			{
+				ascii[(int)str2[i]] = 1;
+				write(1, &str2[i], 1);
+			}
+			++i;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
