c496d6789418a192b434ef1ebaed32e77d0a7e87
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-10 11:59:14 +0000

    Rank 2 : Level 1 : rot13.c Refactor!

--- Level_1/rot13/rot_13.c
+++ Level_1/rot13/rot_13.c
@@ -1,22 +1,34 @@
 #include <unistd.h>
 
+int ft_islower(int c);
+int ft_isupper(int c);
+
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
+	char *str;
+
+	if (argc == 2)
 	{
-		write(1, "\n", 1);
-		return 1;
-	}
-	while (*argv[1])
-	{
-		if ((*argv[1] >= 'a' && *argv[1] <= 'z'))
-			*argv[1] = (*argv[1] - 'a' + 13) % 26 + 'a';
-		else if ((*argv[1] >= 'A' && *argv[3] <= 'Z'))
-			*argv[1] = (*argv[1] - 'A' + 13) % 26 + 'A';
-		write(1, argv[1], 1);
-		++argv[1];
+		str = argv[1];
+		while (*str)
+		{
+			if (ft_islower(*str))
+				*str = (*str - 'a' + 13) % 26 + 'a';
+			else if (ft_isupper(*str))
+				*str = (*str - 'A' + 13) % 26 + 'A';
+			write(1, str++, 1);
+		}
 	}
 	write(1, "\n", 1);
-
 	return 0;
+}
+
+int ft_islower(int c)
+{
+	return ((c >= 'a') && (c <= 'z'));
+}
+
+int ft_isupper(int c)
+{
+	return ((c >= 'A') && (c <= 'Z'));
 }
