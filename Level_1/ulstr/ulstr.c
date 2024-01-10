b17355dc1b5dd7e19955cb299c5aef2da03e2139
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-10 12:08:08 +0000

    Rank 2 : Level 1 : ulstr.c Refactor!

--- Level_1/ulstr/ulstr.c
+++ Level_1/ulstr/ulstr.c
@@ -1,5 +1,8 @@
 #include <unistd.h>
 
+int ft_islower(int c);
+int ft_isupper(int c);
+
 int main(int argc, char **argv)
 {
 	char *str;
@@ -9,13 +12,23 @@ int main(int argc, char **argv)
 		str = argv[1];
 		while (*str)
 		{
-			if (*str >= 'a' && *str <= 'z')
+			if (ft_islower(*str))
 				*str -= 32;
-			else if (*str >= 'A' && *str <= 'Z')
+			else if (ft_isupper(*str))
 				*str += 32;
 			write(1, str++, 1);
 		}
 	}
 	write(1, "\n", 1);
 	return 0;
+}
+
+int ft_islower(int c)
+{
+	return ((c >= 'a') && (c <= 'z'));
+}
+
+int ft_isupper(int c)
+{
+	return ((c >= 'A') && (c <= 'Z'));
 }
