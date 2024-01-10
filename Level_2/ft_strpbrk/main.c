68e30215ae4604538559ed094f0ea6a4b507c375
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-10 12:40:07 +0000

    Rank 2 : Level 2 : ft_strpbrk.c Refactor!

--- Level_2/ft_strpbrk/main.c
+++ Level_2/ft_strpbrk/main.c
@@ -4,12 +4,18 @@
 
 int main(void)
 {
-	char *s = "aaaaZ";
-	char *accept = "Zz Top";
+	char *s = "abaaaZ";
+	char *accept = "Zzb Top";
 	char *found = NULL;
 
+	printf("str address : %p\n", s);
+	printf("str value : %s\n", s);
+
+	printf("accept values : %s\n", accept);
+
 	found = ft_strpbrk(s, accept);
-	printf("%p", found);
+	printf("found : %c\n", *found);
+	printf("at address : %p\n", found);
 	
 	return (0);
 }
