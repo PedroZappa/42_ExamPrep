a2b92c390af2c9c3bf5ce00d5834c36b16ad2b17
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-10 10:32:26 +0000

    Rank 2 : Level 4 : snake_to_camel.c DONE!

---
+++ Level_2/snake_to_camel/snake_to_camel.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int ft_islower(int c);
+int ft_isupper(int c);
+
+int main(int argc, char *argv[])
+{
+	char *str;
+
+	if (argc == 2)
+	{
+		str = argv[1];
+		if (ft_isupper(*str))
+		{
+			*str += 32;
+			write(1, str++, 1);
+		}
+		while (*str)
+		{
+			if (*str == '_')
+			{
+				++str;
+				if (ft_islower(*str))
+					*str -= 32;
+				write(1, str++, 1);
+			}
+			else
+				write(1, str++, 1);
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
+
+int ft_islower(int c)
+{
+	return ((c >= 'a') && (c <= 'z'));
+}
+
+int ft_isupper(int c)
+{
+	return ((c >= 'A') && (c <= 'Z'));
+}
+
