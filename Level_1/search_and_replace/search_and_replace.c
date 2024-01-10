997312571cf54afbb31f6c54ade79189492d4ac2
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-10 12:05:52 +0000

    Rank 2 : Level 1 : search_and_replace.c Refactor!

--- Level_1/search_and_replace/search_and_replace.c
+++ Level_1/search_and_replace/search_and_replace.c
@@ -3,17 +3,19 @@
 int main(int argc, char *argv[])
 {
 	char	*str = argv[1];
-	char	search = argv[2][0];
-	char	replace = argv[3][0];
+	char	search;
+	char	replace;
 
 	if ((argc == 4) && !argv[2][1] && !argv[3][1])
 	{
+		search = argv[2][0];
+		replace = argv[3][0];
+		
 		while (*str)
 		{
 			if (*str == search)
 				*str = replace;
-			write(1, str, 1);
-			++str;
+			write(1, str++, 1);
 		}
 	}
 	write(1, "\n", 1);
