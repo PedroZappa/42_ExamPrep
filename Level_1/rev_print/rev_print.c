d2cd82d44d00b1f72ab11cea482741145a058c43
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-11 15:27:33 +0000

    Level 1 : rev_print : 

--- Level_1/rev_print/rev_print.c
+++ Level_1/rev_print/rev_print.c
@@ -4,12 +4,15 @@ int ft_strlen(char *str);
 
 int main(int argc, char *argv[])
 {
+	char *str;
 	int len;
+
 	if (argc == 2)
 	{
-		len = ft_strlen(argv[1]);
+		str = argv[1];
+		len = ft_strlen(str);
 		while (len >= 0)
-			write(1, &argv[1][len--], 1);
+			write(1, &str[len--], 1);
 	}
 	write(1, "\n", 1);
 	return 0;
