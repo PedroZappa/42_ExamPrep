cfc88e73f1e31db110cdd5b0e6ecb2a481d9968d
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-16 19:41:17 +0000

    Level_4/ft_list_remove_if/ft_list_remove_if.c : fix 

--- Level_4/ft_list_remove_if/ft_list_remove_if.c
+++ Level_4/ft_list_remove_if/ft_list_remove_if.c
@@ -3,9 +3,11 @@
 
 /* Store begin_list in curr;
  * While the current element is not NULL
- *		Compare node->data with data_ref
- *			If they match: free the next node
- * Handle the case where begin_list is equal to data_ref
+ *		Compare next->data with data_ref
+ *			Save the address of next node in 'to_del'
+ *			Get address of curr->next->next node and override curr->next
+ *			free 'to_del'
+ * // Handle the case where begin_list is equal to data_ref
  * Reset curr to begin_list
  * If the current element exists and it's value equals data_ref
  * 		Make the current element point to the next
