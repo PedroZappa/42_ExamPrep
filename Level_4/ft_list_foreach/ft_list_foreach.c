6fcf5b99dfa03044351935ac22ba81250b9e420c
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-16 17:55:09 +0000

    Level_4/ft_list_foreach/ft_list_foreach.c : Annotated 

--- Level_4/ft_list_foreach/ft_list_foreach.c
+++ Level_4/ft_list_foreach/ft_list_foreach.c
@@ -1,5 +1,11 @@
 #include "ft_list.h"
 
+/*	Applies `f` to each element of the list `begin_list` 
+ *
+ *	Loop while `begin_list` is not NULL
+ *		Call `f` on `begin_list->data`
+ *		Get next node in 'begin_list'
+ *	*/
 void    ft_list_foreach(t_list *begin_list, void (*f)(void *))
 {
 	while (begin_list)
--- Level_4/ft_list_foreach/main.c
+++ Level_4/ft_list_foreach/main.c
@@ -4,24 +4,26 @@
 
 void print_data(void *data);
 t_list *create_elem(void *data);
+t_list *add_node(t_list *list, void *data);
 
-int main() {
-    // Create a list
-    t_list *list = create_elem("Hello");
-    list->next = create_elem("World");
-    list->next->next = create_elem("!");
+int main(int argc, char **argv) 
+{
+	int n;
+    t_list *list = create_elem("y0!");
+	n = 1;
+	while (n < argc)
+		list = add_node(list, argv[n++]);
 
     // Apply `print_data` to each element of the list
     ft_list_foreach(list, print_data);
 
     // Free the list
-    t_list *tmp;
+    t_list *lst_start;
     while (list) {
-        tmp = list;
+        lst_start = list;
         list = list->next;
-        free(tmp);
+        free(lst_start);
     }
-
     return 0;
 }
 
@@ -36,7 +38,7 @@ void print_data(void *data)
  * */
 t_list *create_elem(void *data) 
 {
-    t_list *new_elem = (t_list *)malloc(sizeof(t_list));
+    t_list *new_elem = malloc(sizeof(t_list));
     if (!new_elem) 
 		return NULL;
     new_elem->data = data;
@@ -44,3 +46,15 @@ t_list *create_elem(void *data)
     return new_elem;
 }
 
+/*	Add new node to the end of the list 
+ *	*/
+t_list *add_node(t_list *list, void *data) 
+{
+	if (!list) 
+		return create_elem(data);
+	t_list *tmp = list;
+	while (tmp->next) 
+		tmp = tmp->next;
+	tmp->next = create_elem(data);
+	return list;
+}
