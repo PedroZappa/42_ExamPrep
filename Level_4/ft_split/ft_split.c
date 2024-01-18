fc9cbe4765e54a3826952b5cc88837cb57b75e88
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-18 18:46:54 +0000

    Level_4 : ft_split.c : Refactor 

--- Level_4/ft_split/ft_split.c
+++ Level_4/ft_split/ft_split.c
@@ -24,8 +24,8 @@ char    **ft_split(char *str)
 	char **strtab;
 	int str_count;
 	int strlen;
-	int i;		// Input string iterator
-	int j;		// Marks the beginning of a word
+	int i;				// Input string iterator
+	int word_start;		// Marks the beginning of a word
 
 	i = 0;
 	str_count = 0;
@@ -42,23 +42,23 @@ char    **ft_split(char *str)
 	if (!strtab)
 		return (NULL);
 	i = 0;
-	j = 0;
-	str_count = 0;
 	strlen = 0;
+	str_count = 0;
+	word_start = 0;
 	while (str[i])
 	{
 		while (str[i] && ft_isspace(str[i]))
 			++i;
-		j = i;
+		word_start = i;
 		while (str[i] && !ft_isspace(str[i]))
 			++i;
-		if (i > j)
+		if (i > word_start)
 		{
-			strlen = (i - j);
+			strlen = (i - word_start);
 			strtab[str_count] = malloc(sizeof(char) * (strlen + 1));
 			if (!strtab[str_count])
 				return (NULL);
-			ft_strncpy(strtab[str_count++], &str[j], strlen);
+			ft_strncpy(strtab[str_count++], &str[word_start], strlen);
 		}
 	}
 	strtab[str_count] = NULL;
