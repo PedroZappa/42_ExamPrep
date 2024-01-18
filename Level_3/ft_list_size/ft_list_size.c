38ae297b7ecbd7643b6236c5e9878df671c46612
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-18 20:00:12 +0000

    Level_3 : ft_list_size.c : cleanup 󰃢

--- Level_3/ft_list_size/ft_list_size.c
+++ Level_3/ft_list_size/ft_list_size.c
@@ -17,8 +17,8 @@ int	ft_list_size(t_list *begin_list)
 	n_nodes = 0;
 	while (begin_list)
 	{
-		begin_list = begin_list->next;
 		++n_nodes;
+		begin_list = begin_list->next;
 	}
 	return (n_nodes);
 }
