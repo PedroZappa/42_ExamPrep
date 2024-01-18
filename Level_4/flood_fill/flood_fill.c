8586eef1de4ff8399197e29ce51b5d96bb66c11c
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-18 17:20:41 +0000

    Level_1 : flood_fill.c : Refactor 

--- Level_4/flood_fill/flood_fill.c
+++ Level_4/flood_fill/flood_fill.c
@@ -20,8 +20,9 @@ void	fill(char **tab, t_point size, t_point curr, char to_fill)
 {
 	if (out_of_bounds(curr, size) || (tab[curr.y][curr.x] != to_fill))
 		return;
-
+	
 	tab[curr.y][curr.x] = 'F';
+
 	fill(tab, size, (t_point){curr.x - 1, curr.y}, to_fill);
 	fill(tab, size, (t_point){curr.x + 1, curr.y}, to_fill);
 	fill(tab, size, (t_point){curr.x, curr.y - 1}, to_fill);
