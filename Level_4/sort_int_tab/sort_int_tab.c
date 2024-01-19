b2d4934d7cbbb5e8f532a12134b6fbbe838310d8
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-19 11:05:55 +0000

    Level_3 : sort_int_tab.c : cleanup 󰃢

--- Level_4/sort_int_tab/sort_int_tab.c
+++ Level_4/sort_int_tab/sort_int_tab.c
@@ -8,16 +8,16 @@
 void sort_int_tab(int *tab, unsigned int size)
 {
 	unsigned int	n;
-	int				temp;
+	int				to_swap;
 
 	n = 0;
 	while (n < (size - 1))
 	{
 		if (tab[n] > tab[n + 1])
 		{
-			temp = tab[n];
+			to_swap = tab[n];
 			tab[n] = tab[n + 1];
-			tab[n + 1] = temp;
+			tab[n + 1] = to_swap;
 			n = 0;
 		}
 		else
