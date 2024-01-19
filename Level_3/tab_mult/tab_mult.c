e960f1e941999e0184fdfdffe765d482f4d085a9
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-19 11:26:25 +0000

    Level_3 : tab_mult.c : cleanup 󰃢

--- Level_3/tab_mult/tab_mult.c
+++ Level_3/tab_mult/tab_mult.c
@@ -3,6 +3,8 @@
 void ft_putchar(char c);
 void ft_putnbr(int nb);
 int ft_atoi(char *str);
+int ft_isdigit(int c);
+int ft_isspace(int c);
 
 int main(int argc, char *argv[])
 {
@@ -34,7 +36,7 @@ int ft_atoi(char *str)
 	int num = 0;
 	int neg = 1;
 
-	while (*str == ' ' || (*str >= 9 && *str <= 13))
+	while (ft_isspace(*str))
 		++str;
 	if (*str == '-' || *str == '+')
 	{
@@ -42,9 +44,19 @@ int ft_atoi(char *str)
 			neg = -1;
 		++str;
 	}
-	while (*str >= '0' && *str <= '9')
+	while (ft_isdigit(*str))
 		num = (num * 10) + (*str++ - '0');
 	return (num * neg);
+}
+
+int ft_isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+int ft_isspace(int c)
+{
+	return (c == ' ' || (c >= 9 && c <= 13));
 }
 
 void ft_putchar(char c)
