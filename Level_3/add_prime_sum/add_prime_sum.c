df0a438c73d0f1c0b19e52de4ecb200c70e04c43
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-25 18:49:16 +0000

    aad_prime_sum.c : cleaned 󰃢

--- Level_3/add_prime_sum/add_prime_sum.c
+++ Level_3/add_prime_sum/add_prime_sum.c
@@ -70,13 +70,6 @@ int	ft_atoi(char *str)
 
 void	ft_putnbr(int nb)
 {
-	if (nb == -2147483648)
-		write(1, "-2147483648", 11);
-	if (nb < 0)
-	{
-		write(1, "-", 1);
-		nb *= -1;
-	}
 	if (nb >= 10)
 		ft_putnbr(nb / 10);
 	write(1, &"0123456789"[nb % 10], 1);
