5d46aa9ced47fade94f9068675fe9694901e8407
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-25 19:00:36 +0000

    ft_range2.c : cleaned 󰃢

--- Level_3/ft_range/ft_range2.c
+++ Level_3/ft_range/ft_range2.c
@@ -7,28 +7,28 @@ int abs(int n);
  *	Set 'range' to the absolute value of ints in array:
  *	Allocate memory for 'arr':
  *		Protect memory allocation
- *	While 'max' is not equal to 'min'
- *		If 'max' is greater than 'min'
- *			Increment 'max'
+ *	While 'end' is not equal to 'start'
+ *		If 'end' is greater than 'start'
+ *			Increment 'end'
  *		else
- *			Decrement 'min'
+ *			Decrement 'start'
  *		Increment 'arr''s value'
- *	Set the value of the last address pf 'arr' to 'min'
+ *	Set the value of the last address pf 'arr' to 'start'
  *	Return ('arr' - 'range')
  *		This is because the pointer 'arr' has been incremented 'range' times 
  *		so subtracting 'range' brings it back to the start of the range.
  *	*/
-int	*ft_range(int min, int max)
+int	*ft_range(int start, int end)
 {
 	int	range;
 	int	*arr;
 
-	range = abs(max - min);
+	range = abs(end - start);
 	if (!(arr = malloc(sizeof(int) * range)))
 		return (NULL);
-	while (max != min)
-		*arr++ = ((max > min) ? min++ : min--);
-	*arr = min;
+	while (end != start)
+		*arr++ = ((end > start) ? start++ : start--);
+	*arr = start;
 	return (arr - range);
 }
 
