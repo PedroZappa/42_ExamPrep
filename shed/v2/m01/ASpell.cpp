0f0d32e0652fb1eb7165d7dfca4a68d3a69ea3e7
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-04-17 09:41:21 +0100

    feat(cpp_module_01): implement ASpell and Warlock classes
    Introduced ASpell and Warlock classes to enhance spell management and character interaction. This update includes constructors, destructors, and essential methods for handling spell effects and character titles. The main program demonstrates the learning and launching of spells, showcasing the new functionalities.
    
    Signed-off-by: Zedro <45104292+PedroZappa@users.noreply.github.com>

---
+++ shed/v2/m01/ASpell.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ASpell.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/17 09:31:35 by passunca          #+#    #+#             */
+/*   Updated: 2025/04/17 09:40:14 by passunca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ASpell.hpp"
+
+ASpell::ASpell(std::string const &name, std::string const &effects)
+	: _name(name), _effects(effects) {
+}
+ASpell::~ASpell() {
+}
+
+ASpell::ASpell(ASpell const &ref) {
+	*this = ref;
+}
+
+ASpell &ASpell::operator=(ASpell const &ref) {
+	if (this != &ref) {
+		_name = ref.getName();
+	}
+}
+
+std::string ASpell::getName() const {
+	return (_name);
+}
+std::string ASpell::getEffects() const {
+	return (_effects);
+}
+
+// void launch(ATarget const &target) const;
---
+++ shed/v2/m01/Warlock.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Warlock.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/17 09:16:11 by passunca          #+#    #+#             */
+/*   Updated: 2025/04/17 09:26:10 by passunca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Warlock.hpp"
+
+Warlock::Warlock(std::string name, std::string title)
+	: _name(name), _title(title) {
+	std::cout << getName() << ": This looks like another boring day."
+			  << std::endl;
+}
+
+Warlock::~Warlock() {
+	std::cout << getName() << ": My job here is done!" << std::endl;
+}
+
+std::string Warlock::getName() const {
+	return (_name);
+}
+
+std::string Warlock::getTitle() const {
+	return (_title);
+}
+
+void Warlock::setTitle(std::string const &title) {
+	_title = title;
+}
+
+void Warlock::introduce() const {
+	std::cout << getName() << ": I am " << getName() << ", " << getTitle()
+			  << std::endl;
+}
