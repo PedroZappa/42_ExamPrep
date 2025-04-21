19363e55e3ae48dcef2d43d294407d0c9c3db548
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-04-21 15:25:12 +0100

    feat(spell): add Polymorph class implementation
    Introduce the Polymorph class derived from ASpell, complete with constructor, destructor, and clone method. This addition expands the spell system by providing a new spell type, enhancing the variety of spells available.

---
+++ shed/v3/m02/Polymorph.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Polymorph.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/21 15:23:33 by passunca          #+#    #+#             */
+/*   Updated: 2025/04/21 15:23:56 by passunca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include "ASpell.hpp"
+
+class Polymorph : public ASpell {
+  public:
+	Polymorph();
+	~Polymorph();
+
+	ASpell *clone() const;
+};
+
+
