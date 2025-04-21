348d67b1927450bafe47a4145c2740a8c6b73091
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-04-21 08:56:06 +0100

    refactor(target): update BrickWall description
    Changed the description of BrickWall from 'Inconspicuous Brick Wall' to 'Inconspicuous Red-brick Wall' to better reflect its appearance and characteristics.
    
    Signed-off-by: Zedro <45104292+PedroZappa@users.noreply.github.com>

--- shed/v2/m02/BrickWall.cpp
+++ shed/v2/m02/BrickWall.cpp
@@ -12,7 +12,7 @@
 
 #include "BrickWall.hpp"
 
-BrickWall::BrickWall() : ATarget("Inconspicuous Brick Wall") {
+BrickWall::BrickWall() : ATarget("Inconspicuous Red-brick Wall") {
 }
 
 BrickWall::~BrickWall() {
