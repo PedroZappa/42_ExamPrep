236b1b608198463a77fb705de3c0ef7a91639754
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-05-05 07:50:55 +0000

    feat(ASpell): implement ASpell class and update main
    Added ASpell class with constructors, destructors, and methods for managing spell names and effects. Updated main.cpp to demonstrate spell learning and launching functionality.
    
    Signed-off-by: Zedro <45104292+PedroZappa@users.noreply.github.com>

--- shed/v6/m01/main.cpp
+++ shed/v6/m01/main.cpp
@@ -1,18 +1,20 @@
 #include "Warlock.hpp"
+#include "Fwoosh.hpp"
+#include "Dummy.hpp"
 
 int main()
 {
-  Warlock const richard("Richard", "Mistress of Magma");
+  Warlock richard("Richard", "the Titled");
+
+  Dummy bob;
+  Fwoosh* fwoosh = new Fwoosh();
+
+  richard.learnSpell(fwoosh);
+
   richard.introduce();
-  std::cout << richard.getName() << " - " << richard.getTitle() << std::endl;
+  richard.launchSpell("Fwoosh", bob);
 
-  Warlock* jack = new Warlock("Jack", "the Long");
-  jack->introduce();
-  jack->setTitle("the Mighty");
-  jack->introduce();
-
-  delete jack;
-
-  return (0);
+  richard.forgetSpell("Fwoosh");
+  richard.launchSpell("Fwoosh", bob);
 }
 
