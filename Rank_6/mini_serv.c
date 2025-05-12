10c8f6516c2a349a34d2dc949285bf5a3913a92a
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-05-12 12:40:19 +0100

    refactor(mini_serv): improve code readability and modularity
    Refactored  to enhance code clarity and maintainability:
    
    - Added comments to explain constants and conditions.
    
    - Modularized functions such as , , , , and  for better separation of concerns.
    
    - Fixed a bug in  by correcting the parameter type.
    
    These changes improve the overall structure and readability of the code, making it easier to maintain and extend.
    
    Signed-off-by: Zedro <45104292+PedroZappa@users.noreply.github.com>

--- Rank_6/mini_serv.c
+++ Rank_6/mini_serv.c
@@ -14,7 +14,7 @@ void fatal(void);
 void broadcast(int client_fd, int max_fd, fd_set write_fd_set, char *m);
 void clean(int max_fd, t_client *cls, fd_set active_fd_set);
 int extract_msg(char **buf, char **msg);
-char *str_join(char **buf, char *add);
+char *str_join(char *buf, char *add);
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -39,9 +39,9 @@ int main(int argc, char **argv) {
   if (listen(sockfd, 10) != 0)
     fatal();
 
-  const int BSIZE = 1024;
-  const int MSIZE = 64;
-  const int CSIZE = (16 * 4096);
+  const int BSIZE = 1024;        // Buffer Size
+  const int MSIZE = 64;          // Message Size
+  const int CSIZE = (16 * 4096); // Client Size
 
   t_client cls[CSIZE];
   fd_set active_fd_set, read_fd_set, write_fd_set;
@@ -80,10 +80,10 @@ int main(int argc, char **argv) {
           char buff[BSIZE];
           bzero(buff, BSIZE);
           int bytes = recv(client_fd, buff, (BSIZE - 1), 0);
-          if (bytes <= 0) {
+          if (bytes <= 0) { // No message
             char m[MSIZE];
             bzero(m, MSIZE);
-            sprintf(m, "server: client %d just left\n", cls[client_fd]);
+            sprintf(m, "server: client %d just left\n", cls[client_fd].id);
 
             broadcast(client_fd, max_fd, write_fd_set, m);
             FD_CLR(client_fd, &active_fd_set);
@@ -92,16 +92,16 @@ int main(int argc, char **argv) {
               free(cls[client_fd].m);
               cls[client_fd].m = NULL;
             }
-          } else {
+          } else { // Has message
             char *line = NULL;
             cls[client_fd].m = str_join(cls[client_fd].m, buff);
             while (extract_msg(&(cls[client_fd].m), &line)) {
-            char m[MSIZE + strlen(line)];
-            bzero(m, MSIZE + strlen(line));
-            sprintf(m, "client %d: %s", cls[client_fd].id, line);
-							broadcast(client_fd, max_fd, write_fd_set, m);
-							free(line);
-							line = NULL;
+              char m[MSIZE + strlen(line)];
+              bzero(m, MSIZE + strlen(line));
+              sprintf(m, "client %d: %s", cls[client_fd].id, line);
+              broadcast(client_fd, max_fd, write_fd_set, m);
+              free(line);
+              line = NULL;
             }
             if (cls[client_fd].m[0] == '\0') {
               free(cls[client_fd].m);
@@ -112,6 +112,75 @@ int main(int argc, char **argv) {
       }
     }
   }
-	clean(max_fd, cls, active_fd_set);
+  clean(max_fd, cls, active_fd_set);
   return (0);
+}
+
+void fatal(void) {
+  write(2, "Fatal error\n", 12);
+  exit(1);
+}
+
+void broadcast(int client_fd, int max_fd, fd_set write_fd_set, char *m) {
+  for (int broadcast_fd = 0; broadcast_fd <= max_fd; ++broadcast_fd) {
+    if (FD_ISSET(broadcast_fd, &write_fd_set) && (broadcast_fd != client_fd)) {
+      send(broadcast_fd, m, strlen(m), 0);
+    }
+  }
+}
+
+void clean(int max_fd, t_client *cls, fd_set active_fd_set) {
+  for (int client_fd = 0; client_fd <= max_fd; ++client_fd) {
+    if (FD_ISSET(client_fd, &active_fd_set)) {
+      FD_CLR(client_fd, &active_fd_set);
+      close(client_fd);
+    }
+    if (cls[client_fd].m) {
+      free(cls[client_fd].m);
+      cls[client_fd].m = NULL;
+    }
+  }
+}
+
+int extract_message(char **buf, char **msg) {
+  char *newbuf;
+  int i;
+
+  *msg = 0;
+  if (*buf == 0)
+    return (0);
+  i = 0;
+  while ((*buf)[i]) {
+    if ((*buf)[i] == '\n') {
+      newbuf = calloc(1, sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
+      if (newbuf == 0)
+        return (-1);
+      strcpy(newbuf, *buf + i + 1);
+      *msg = *buf;
+      (*msg)[i + 1] = 0;
+      *buf = newbuf;
+      return (1);
+    }
+    i++;
+  }
+  return (0);
+}
+
+char *str_join(char *buf, char *add) {
+  char *newbuf;
+  int len;
+
+  if (buf == 0)
+    len = 0;
+  else
+    len = strlen(buf);
+  newbuf = malloc(sizeof(*newbuf) * (len + strlen(add) + 1));
+  if (newbuf == 0)
+    return (0);
+  newbuf[0] = 0;
+  if (buf != 0)
+    strcat(newbuf, buf);
+  free(buf);
+  strcat(newbuf, add);
+  return (newbuf);
 }
